dbcc9818d0df68ed1a00be44e6d4b13b505c3b29
Marcello Lopes <marcell0lopes@outlook.com>
2022-07-14 00:30:37 -0300

    added custom font (poppins)

--- App.tsx
+++ App.tsx
@@ -1,6 +1,35 @@
 import React from "react";
+
+import { ThemeProvider } from "styled-components";
+import * as SplashScreen from "expo-splash-screen";
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+import theme from "./src/globals/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
-  return <Dashboard />;
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  SplashScreen.hideAsync();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
