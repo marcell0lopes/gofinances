b4504f74edebc389f2b7a9cd26f3ad90a647b197
Marcello Lopes <marcell0lopes@outlook.com>
2022-07-13 23:32:43 -0300

    add styled components

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dashboard } from "./src/screens/Dashboard/Dashboard";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return <Dashboard />;
