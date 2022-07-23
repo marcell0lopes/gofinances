1dfb9d95e1398a564fe7b49a89e2b02ce0acfb4c
Marcello Lopes <marcell0lopes@outlook.com>
2022-07-23 20:16:41 -0300

    create category modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,13 @@
 import React, { useState } from "react";
+import { Modal } from "react-native";
+
+import { Input } from "../../components/Form/Input";
+import { Button } from "../../components/Form/Button";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
+
+import { CategorySelect } from "../CategorySelect";
+
 import {
   Container,
   Title,
@@ -7,15 +16,25 @@ import {
   Fields,
   TransactionsTypes,
 } from "./styles";
-import { Input } from "../../components/Form/Input";
-import { Button } from "../../components/Form/Button";
-import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 
 export function Register() {
+  const [category, setCategory] = useState({
+    key: "category",
+    name: "Categoria",
+  });
   const [transactionType, setTransactionType] = useState("");
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
   function handleTransactionTypeSelect(type: "up" | "down") {
     setTransactionType(type);
+  }
+
+  function handleOpenSelectCategoryModal() {
+    setCategoryModalOpen(true);
+  }
+
+  function handleCloseSelectCategoryModal() {
+    setCategoryModalOpen(false);
   }
 
   return (
@@ -41,10 +60,22 @@ export function Register() {
               isActive={transactionType === "down"}
             />
           </TransactionsTypes>
+          <CategorySelectButton
+            title={category.name}
+            onPress={handleOpenSelectCategoryModal}
+          />
         </Fields>
 
         <Button title="Enviar" />
       </Form>
+
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseSelectCategoryModal}
+        />
+      </Modal>
     </Container>
   );
 }
