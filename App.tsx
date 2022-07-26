ddabea913625a2cddb2cd38b5dc2548b0a2dfacc
Marcello Lopes <marcell0lopes@outlook.com>
2022-07-26 19:55:14 -0300

    Add login screen

--- App.tsx
+++ App.tsx
@@ -19,6 +19,8 @@ import theme from "./src/globals/styles/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./src/routes/app.routes";
 
+import { SignIn } from "./src/screens/SignIn";
+
 export default function App() {
   SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({
@@ -38,7 +40,8 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar barStyle="light-content" />
-          <AppRoutes />
+          {/* <AppRoutes /> */}
+          <SignIn />
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
---
+++ src/components/SignInSocialButton/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, ImageContainer, Title } from "./styles";
+
+import { RectButtonProps } from "react-native-gesture-handler";
+import { SvgProps } from "react-native-svg";
+
+interface Props extends RectButtonProps {
+  title: string;
+  svg: React.FC<SvgProps>;
+}
+
+export function SignInSocialButton({ title, svg: Svg, ...rest }: Props) {
+  return (
+    <Button>
+      <ImageContainer>
+        <Svg />
+      </ImageContainer>
+      <Title>{title}</Title>
+    </Button>
+  );
+}
