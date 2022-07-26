6125480318230294f7c2abbcb998cbf315df168f
Marcello Lopes <marcell0lopes@outlook.com>
2022-07-26 17:10:07 -0300

    fix type error

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -2,7 +2,10 @@ import styled from "styled-components/native";
 import { FlatList, FlatListProps } from "react-native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from "@expo/vector-icons";
-import { BorderlessButton } from "react-native-gesture-handler";
+import {
+  BorderlessButton,
+  BorderlessButtonProps,
+} from "react-native-gesture-handler";
 
 import {
   getBottomSpace,
@@ -10,6 +13,11 @@ import {
 } from "react-native-iphone-x-helper";
 
 import { DataListProps } from ".";
+import { ReactNode } from "react";
+
+interface LogoutButtonProps extends BorderlessButtonProps {
+  children: ReactNode;
+}
 
 export const Container = styled.View`
   flex: 1;
